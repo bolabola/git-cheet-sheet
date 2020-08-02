e1f297b3393a150b0ae3a1b9051514623c1efd36
lesiry <lesiry@gmail.com>
2020-08-02 13:06:38 +0800

    Update git-cheet-sheet.sh
    proxy

--- git-cheet-sheet.sh
+++ git-cheet-sheet.sh
@@ -117,6 +117,19 @@ $ git clean -f -d -x                                          # same as above bu
 
 $ git blame <file>                                            # list the change dates and authors for a file
 
+------------------------------proxy------------------------------
+$ git config --global http.proxy
+$ git config --global https.proxy
+
+$ git config --global --unset http.proxy
+$ git config --global --unset https.proxy
+
+$ git config --global http.proxy 'http://127.0.0.1:1080'
+$ git config --global https.proxy 'http://127.0.0.1:1080'
+$ git config --global http.proxy 'socks5://127.0.0.1:1080'
+$ git config --global https.proxy 'socks5://127.0.0.1:1080'
+
+
 # check for errors and cleanup repository
 $ git fsck
 $ git gc --p
